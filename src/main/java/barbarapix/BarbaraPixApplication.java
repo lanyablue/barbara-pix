9b8ed4a0e1d3d218996821f5e76b7f1c31a16c4b
lanyablue <barbaramy_@hotmail.com>
2024-01-14 16:42:31 -0300

    first commit

---
+++ src/main/java/barbarapix/BarbaraPixApplication.java
@@ -0,0 +1,13 @@
+package barbarapix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbaraPixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbaraPixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/barbarapix/BarbaraPixApplicationTests.java
@@ -0,0 +1,13 @@
+package barbarapix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbaraPixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
